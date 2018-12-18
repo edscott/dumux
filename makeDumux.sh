4a47e419304a0870278177576bcece053dcc2bf8
edscott wilson garcia <edscott@imp.mx>
2018-12-18 16:11:36 -0600

    variable release versions in script

--- makeDumux.sh
+++ makeDumux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+DUNE_RELEASE=2.4
+DUMUX_RELEASE=2.12
 echo "# Dune core modules:"; \
     modules='dune-common dune-geometry dune-grid dune-localfunctions dune-istl'; \
     cd /usr/local/src/dune; \
@@ -7,7 +8,7 @@ echo "# Dune core modules:"; \
         if test -d $module; then \
             rm -rf $module; \
         fi; \
-        git clone -b releases/2.5 \
+        git clone -b releases/$DUNE_RELEASE \
         https://gitlab.dune-project.org/core/$module.git $module; \
     done; \
     echo "# Dune staging modules:"; \
@@ -17,7 +18,7 @@ echo "# Dune core modules:"; \
         if test -d $module; then \
             rm -rf $module; \
         fi; \
-        git clone -b releases/2.5 \
+        git clone -b releases/$DUNE_RELEASE \
         https://gitlab.dune-project.org/staging/$module.git $module; \
     done; \
     echo "# Dune extension modules: "; \
@@ -27,80 +28,80 @@ echo "# Dune core modules:"; \
         if test -d $module; then \
             rm -rf $module; \
         fi; \
-        git clone -b releases/2.5 \
+        git clone -b releases/$DUNE_RELEASE \
         https://gitlab.dune-project.org/extensions/$module.git $module; \
     done; \
     echo "# Configure"; \
     echo "# Step by step for build tests:"; \
     p='dune-common'; \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
-    done && dune-common/bin/dunecontrol --only=$p  --opts=dune-2.5.opts make; \
+    done && dune-common/bin/dunecontrol --only=$p  --opts=dune-$DUNE_RELEASE.opts make; \
     p='dune-geometry'; \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
-    done && dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make; \
+    done && dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make; \
     p='dune-uggrid'; \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
-    done && dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make; \
+    done && dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make; \
     p='dune-grid'; \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
-    done && dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make; \
+    done && dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make; \
     p='dune-localfunctions'; \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
-    done && dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make; \
+    done && dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make; \
     p='dune-istl'; \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
-    done && dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make; \
-    echo "# releases/2.5 does not compile with gcc7, we are using branch master from jan-11-2018"; \
+    done && dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make; \
+    echo "# releases/$DUNE_RELEASE does not compile with gcc7, we are using branch master from jan-11-2018"; \
     p='dune-alugrid'; \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
-    done && dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make;
+    done && dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make;
 
 cd /usr/local/src/dune && \
     touch dune-geometry/doc/refinement/refinement_safepdf ; \
     touch /usr/local/src/dune/dune-geometry/build-cmake/doc/refinement/refinement.pdf ;\
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts make install
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts make install
 modules='dune-fem'; \
     cd /usr/local/src/dune; \
     for module in $modules; do \
         if test -d $module; then \
             rm -rf $module; \
         fi; \
-        git clone -b releases/2.5 \
+        git clone -b releases/$DUNE_RELEASE \
         https://gitlab.dune-project.org/dune-fem/$module.git $module; \
     done;\
 echo "# Jed Brown workaround hack"; 
@@ -108,28 +109,28 @@ cp DuneFemMacros.cmake.dune-fem \
     /usr/local/src/dune/dune-fem/cmake/modules/DuneFemMacros.cmake
 p='dune-fem'; \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
     done &&  \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make install
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make && \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make install
 modules='dune-functions'; \
     cd /usr/local/src/dune; \
     for module in $modules; do \
         if test -d $module; then \
             rm -rf $module; \
         fi; \
-        git clone -b releases/2.5 \
+        git clone -b releases/$DUNE_RELEASE \
         https://gitlab.dune-project.org/staging/$module.git $module; \
     done;
 modules='dune-pdelab'; \
     cd /usr/local/src/dune; \
     for module in $modules; do \
         if test -d $module; then rm -rf $module; fi; \
-        git clone -b releases/2.5 \
+        git clone -b releases/$DUNE_RELEASE \
         https://gitlab.dune-project.org/pdelab/$module.git $module; \
     done;
 modules='dune-typetree'; \
@@ -138,41 +139,41 @@ modules='dune-typetree'; \
         if test -d $module; then \
             rm -rf $module; \
         fi; \
-        git clone -b releases/2.5 \
+        git clone -b releases/$DUNE_RELEASE \
         https://github.com/dune-project/$module.git $module; \
     done;
 p='dune-typetree'; \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
     done &&  \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make install
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make && \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make install
 p='dune-functions'; \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
     done && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make install
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make && \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make install
 cd /usr/local/src/dune; \
-    git clone -b releases/2.12 \
+    git clone -b releases/$DUMUX_RELEASE \
     https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git dumux/;
 p='dumux';  \
     cd /usr/local/src/dune && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p configure; \
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p configure; \
     files=`find /usr/local/src/dune -name flags.make`; \
     for f in $files; do  \
         sed 's/-D-pthread//' <$f >$f.new && \
         mv $f.new $f ; \
     done && \
-    dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make \
-    && dune-common/bin/dunecontrol  --opts=dune-2.5.opts --only=$p make install
+    dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make \
+    && dune-common/bin/dunecontrol  --opts=dune-$DUNE_RELEASE.opts --only=$p make install
 rsync -av /usr/local/src/dune/dumux/dumux/common/ /opt/dune/include/dumux/common/    
 
